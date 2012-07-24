ce9e99be241d4aeed7d7502ffa71a409324ed2fa
priya <ganti.priya@gmail.com>
2012-07-23 22:28:34 -0700

    version 1

---
+++ StarRhombus.java
@@ -0,0 +1,15 @@
+
+public class StarRhombus {
+	
+	public static void main(String[] args)
+	{
+		int starVertical;
+	
+		for(starVertical = 0; starVertical < 10; starVertical++ )
+		{
+			System.out.println("**********");
+		}
+	}
+
+
+}
